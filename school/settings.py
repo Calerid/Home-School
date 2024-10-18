7c8e5bc3ab0d2bbd7ed42efea82c31b4308c192d
Michael Braley <michael.parker.braley@gmail.com>
2024-10-18 15:41:58 -0500

    Added Template & CSS Directory
    Added a directory for both Templates & CSS under the website app. Added the template path to the project settings.py file.

--- school/settings.py
+++ school/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'school.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['website/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
